2bb475b15130539bbc2eed23ed4d97e4cca3c9f1
Thanos Pasias <thanospas@yahoo.com>
2020-03-29 01:13:00 +0100

    New summary test file

---
+++ Thanos_test_regression.py
@@ -0,0 +1,163 @@
+
+
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+
+#DATA PRE-PROCESSING
+
+
+# Reading the dataset data and start of the pre-processing 
+heart_data = pd.read_excel('Dataset.xlsx')
+ 
+# We need to conduct an one-out-of-K transformation to the famhist column of
+# data into a binary 0,1 form that is suitable to get fed into the PCA analysis
+fam_history = heart_data['famhist']
+unique_hist = np.unique(fam_history)
+
+# We have created a dictionary where the attribute Present/Absent was
+# codified into 0,1
+historyDict = dict(zip(fam_history,[1,0]))
+
+# The transformed array replaces the famhist values
+fh = np.array([historyDict[cl] for cl in fam_history])
+binary_heart_data = heart_data.copy()
+binary_heart_data.famhist = fh
+y = heart_data[['chd']].to_numpy().squeeze()
+binary_heart_data.drop('row.names', axis=1, inplace=True)
+binary_heart_data.drop('chd', axis=1, inplace=True)
+
+# Data standardization: We scale our data so that each feature has
+# a single unit of variance.
+scaler = StandardScaler()
+scaler.fit(binary_heart_data)
+X = scaler.transform(binary_heart_data)   # What about y?s
+
+# Non-standardized data
+Xns = binary_heart_data.to_numpy()
+
+# Clean up variables
+del scaler, fam_history, fh, heart_data, unique_hist, historyDict
+
+
+#---------------------------------------------
+#MODEL TRAINING
+
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X_train, y_train)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 3)
+X_poly = poly_reg.fit_transform(X_train)
+poly_reg.fit(X_poly, y_train)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y_train)
+
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+
+#-----------------------------------------------------------
+#PREDICTIONS
+
+
+
+# Predicting a new result with Linear Regression
+lin_pred = lin_reg.predict(X_test)
+
+# Predicting a new result with Polynomial Regression
+poly_pred= lin_reg_2.predict(poly_reg.fit_transform(X_test))
+
+
+# Predicting the Test set results
+log_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, log_pred)
+
+#---------------------------------------------------------
+#VISUALIZATIONS
+
+
+
+'''# Visualising the Linear Regression results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X, lin_reg.predict(X_train), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, lin_reg_2.predict(poly_reg.fit_transform(X_train), color = 'blue')
+plt.title('Truth or Bluff Polynomial Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X_train), max(X_train), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01) )
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()'''
+
