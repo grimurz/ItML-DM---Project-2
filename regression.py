e413f9bcf52d03f296db1ff6d6c870a9381ea301
Grímur Zimsen <grimurz@gmail.com>
2020-03-29 17:13:24 +0000

    Regression part B WIP

--- regression.py
+++ regression.py
@@ -15,24 +15,24 @@ from sklearn.pipeline import make_pipeline
 
 # Prepare data
 y = np.array(X[:,3]).reshape(-1,1) # y = adiposity
-# X = X[:,[0,1,2, 4,5,6,7,8]] # exclude adiposity
-# X = X[:,[1,2, 4,5,6,7,8]]  # 0: 0.537
-# X = X[:,[2,4, 5,6,7,8]]  # 1: 0.5353
-# X = X[:,[2, 5,6,7,8]]  # 4/5: 0.5346
-# X = X[:,[2, 6,7,8]]  # 5: 0.534   <---  best linreg according to primitive backwards selection
-X = X[:,[2, 6,8]]  # 7: 0.5345   <---  best ridgereg (3rd order poly)
-# X = X[:,[ 6,8]]  # 2: 0.5444
-# X = X[:,[ 6 ]]  # 8: 0.6955
+# X1 = X[:,[0,1,2, 4,5,6,7,8]] # exclude adiposity
+# X1 = X[:,[1,2, 4,5,6,7,8]]  # 0: 0.537
+# X1 = X[:,[2,4, 5,6,7,8]]  # 1: 0.5353
+# X1 = X[:,[2, 5,6,7,8]]  # 4/5: 0.5346
+X1 = X[:,[2, 6,7,8]]  # 5: 0.534   <---  best linreg according to primitive backwards selection
+X2 = X[:,[2, 6,8]]  # 7: 0.5345   <---  best ridgereg (3rd order poly)
+# X2 = X[:,[ 6,8]]  # 2: 0.5444
+# X2 = X[:,[ 6 ]]  # 8: 0.6955
 
 
-### Regression, part a ###
+### REGRESSION, PART A ###
 
 #%% 1. Adiposity because it's well correlated? Show that we can use regression
 #      to impute missing data?
 
 # All attributes are used. Do we want some improved attribute selection?
 # Standardization should strictly speaking happen within each fold. Not sure
-# if it really matters in our case
+# if it really matters in our case. SWITCH FROM RMSE TO MSE?
 
 # K-fold crossvalidation
 K = 10
@@ -42,11 +42,11 @@ CV = model_selection.KFold(n_splits=K, shuffle=True, random_state=42)
 rmse = np.zeros(K)
 
 k=0
-for train_index, test_index in CV.split(X):
+for train_index, test_index in CV.split(X1):
 
     # extract training and test set for current CV fold
-    X_train, y_train = X[train_index,:], y[train_index]
-    X_test, y_test = X[test_index,:], y[test_index]
+    X_train, y_train = X1[train_index,:], y[train_index]
+    X_test, y_test = X1[test_index,:], y[test_index]
 
     # Fit ordinary least squares regression model
     lin_reg = lm.LinearRegression(fit_intercept=True)
@@ -69,8 +69,6 @@ del rmse
 
 #%% 2. 3rd order poly (Two-layer CV?)
 
-# X2, X_te, y2, y_te = train_test_split(X, y, test_size = 0.2, random_state = 42)
-
 # Values of lambda
 lambdas = np.logspace(-3, 4, 50)
 
@@ -84,13 +82,13 @@ rmse_test = np.zeros([K,len(lambdas)])
 
 k=0
 # for train_index, test_index in CV.split(X2):
-for train_index, test_index in CV.split(X):
+for train_index, test_index in CV.split(X2):
 
     # extract training and test set for current CV fold
     # X_train, y_train = X2[train_index,:], y2[train_index]
     # X_test, y_test = X2[test_index,:], y2[test_index]
-    X_train, y_train = X[train_index,:], y[train_index]
-    X_test, y_test = X[test_index,:], y[test_index]
+    X_train, y_train = X2[train_index,:], y[train_index]
+    X_test, y_test = X2[test_index,:], y[test_index]
 
     # Fit for each lambda
     for i, lam in enumerate(lambdas):
@@ -156,9 +154,81 @@ vi   profit???
 https://scikit-learn.org/stable/auto_examples/linear_model/plot_polynomial_interpolation.html
 '''
 
-### Regression, part b ###
+
+### REGRESSION, PART B ###
+
 #%% 1.
 
+K1 = 10
+K2 = 10
+
+#lambdas = ???
+#hidden_units = ???
+
+# Init errors
+ann_error = np.zeros(K1) # articial neural network
+rr_error = np.zeros(K1)  # ridge regression
+bl_error = np.zeros(K1)  # baseline
+
+
+CV1 = model_selection.KFold(n_splits=K1, shuffle=True, random_state=42)
+CV2 = model_selection.KFold(n_splits=K2, shuffle=True, random_state=42)
+
+# Outer CV for test data
+k=0
+for par_index, test_index in CV1.split(X):
+
+    # extract training and test set for current CV fold
+    X_par, y_par = X[par_index,:], y[par_index]
+    X_test, y_test = X[test_index,:], y[test_index]
+
+    # Inner loop for training and validation
+    j=0
+    for train_index, val_index in CV2.split(X_par):
+    
+        # extract training and test set for current CV fold
+        X_train, y_train = X_par[train_index,:], y_par[train_index]
+        X_val, y_val = X_par[val_index,:], y_par[val_index]
+
+        # crunch that sweet training data
+
+        # ANN
+            
+            
+        # Ridge
+
+
+
+        
+        # validate that shiz and find those optimal hypers
+        
+        j+=1
+    
+
+    ### use test data here ###
+
+    # ANN
+        
+        
+    # Ridge
+
+
+    # Baseline
+    y_bl_pred = np.mean(y_test)
+    bl_error[k] = np.mean((y_bl_pred-y_test)**2)  # mean square error
+
+    k+=1
+    
+    
+# Take mean from all three methods and compare?
+
+
+
+del j, k, K1, K2, y_bl_pred
+del par_index, train_index, val_index, test_index
+
+
+#%% 2.
 
 
 
@@ -167,6 +237,8 @@ https://scikit-learn.org/stable/auto_examples/linear_model/plot_polynomial_inter
 
 
 
+
+#%% 3.
 
 
 
@@ -176,6 +248,9 @@ https://scikit-learn.org/stable/auto_examples/linear_model/plot_polynomial_inter
 
 
 #%% Copy/pase bin, junk and other stuff
+
+
+# X2, X_te, y2, y_te = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
 # X = X[np.argsort(X[:, 6])] # line plot gets otherwise messy
 
